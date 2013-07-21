2f64fa02bf46f9f09f5476ffd87defb7a6387a1a
Andrew Kennedy <akennedy2292@gmail.com>
2013-07-21 16:26:26 -0400

    better bindings. added dependency checking

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,58 @@
 var script = document.createElement('script');
 
 function twitstagramClosure() {
-  // twitter doesn't load all tweets at once, so we need to re-bind
-  // click events as more tweets are loaded.
-  // TODO: better method of re-binding than setInterval
-  setInterval(function() { 
-    // unbind all clicks twistagram applies (twitter has it's own binding system,
-    // so this unbind doesn't break anything. if there are other extensions running 
-    // on the page however, it might break those. TODO better binding)
-    $(".expanding-stream-item").unbind("click");
-    // bind to when a user expands a tweet
-    $(".expanding-stream-item").click(function() {
+  // checking whether dependencies exist (thanks @theabraham)
+  function ready(callback) { 
+    var wait = function() {
+      setTimeout(function() {
+        ready.call(null, callback); 
+      }, 500); 
+    };
+
+    try {
+      ( 
+           isUndefined(window.$)
+        || isUndefined(window.jQuery)
+      ) ? wait() : setTimeout(function() { callback(); }, 3e3);
+    } catch (err) {
+      wait();
+    }
+  }
+
+  function isUndefined(value) {
+    return typeof value === 'undefined';
+  }
+
+  // once dependencies have been loaded, set bindings
+  ready(function() {
+    $('#page-container').on('click', '.expanding-stream-item', function(e) {
       var urlInTweet = $(this).find(".js-display-url").html();
       // check if there's a url in the tweet, it contains a link to instagram, and that
       // instagram picture hasn't already been added.
       if (urlInTweet && urlInTweet.indexOf("instagram") != -1 && 
-          !$(this).find(".expanded-content").children(".twitstagram").length) {
+          !$(this).find(".twitstagram").length) {
+        // use instagram api to get direct image src
         var imgSrc = "http://instagr.am/p/" + urlInTweet.split("/")[2] + "/media/";
-        $(this).find(".expanded-content").append("<img class='twitstagram' src='" + imgSrc + "' />");
-      }
+        // if http or https isn't in the url, prepend it so we have a real link
+        urlInTweet = (/^(http|https)/.exec(urlInTweet)) ? urlInTweet : "http://" + urlInTweet;
+        var html = ' \
+          <div class="card2 twitstagram" data-card2-name="photo"> \
+            <div class="card2-photo card2-holder" data-card2-url="' + urlInTweet + '"> \
+              <div class="card2-image-wrapper"> \
+                <img class="card2-image-pic" src="' + imgSrc + '" alt="" /> \
+              </div> \
+              <div class="card2-footer"> \
+              <a href="' + urlInTweet + '" target="_blank" class="js-card2-external-link"> \
+                View on web \
+              </a> \
+            </div> \
+           </div> \
+          </div>';
+        // add instagram image to the top of the 'details' div
+        $(this).find(".js-tweet-details-fixer.tweet-details-fixer").prepend(html);
+       }
     });
-  }, 1000);
+  });
 }
 // add source code to script element 
 script.textContent = ";(" + twitstagramClosure.toString() + ")();";
